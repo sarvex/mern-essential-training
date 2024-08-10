ca4a5b34aa66c3168402576fcb3eaeab6fc82765
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-08-10 23:43:16 +0530

    Started Player form

--- app/src/components/player/PlayerForm.jsx
+++ app/src/components/player/PlayerForm.jsx
@@ -1,5 +1,39 @@
-const PlayerForm = () => {
-  return <div>PlayerForm</div>
+import React from 'react'
+import axios from 'axios'
+
+class PlayerForm extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        <h1 className="center">Add a new Player</h1>
+        <form action="" className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <input id="firstName" ref="firstName" type="text" />
+              <label htmlFor="firstName">First Name</label>
+            </div>
+            <div className="input-field col s6">
+              <input id="lastName" ref="lastName" type="text" />
+              <label htmlFor="lastName">Last Name</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s6">
+              <input id="phone" ref="phone" type="text" />
+              <label htmlFor="phone">Phone</label>
+            </div>
+            <div className="input-field col s6">
+              <input id="email" ref="email" type="text" />
+              <label htmlFor="email">email</label>
+            </div>
+          </div>
+          <button className="btn waves-effect waves-light" type="submit" name="action">
+            Add Player
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default PlayerForm
