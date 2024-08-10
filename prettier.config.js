e5c92d57e6d0be911f1d524ec859c2e6a6ba5beb
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-08-10 16:22:29 +0530

    added frontend app

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
+
+module.exports = config
